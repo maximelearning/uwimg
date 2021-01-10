4c90c3aee73ec41b1128c2d718c1a00746fbbb9e
Estevan Seyfried <estevanseyfried@gmail.com>
2021-01-10 14:53:20 -0800

    Implement get/set pixel and .py tests

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -4,21 +4,36 @@
 #include <math.h>
 #include "image.h"
 
+// Helper Methods
+int get_index(image im, int x, int y, int c);
+
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+    // Do bound checking on c?
+    if (c < 0 || c >= im.c) return 0;
+
+    // Use Clamp padding
+    x = MIN(MAX(0,x), im.w - 1);
+    y = MIN(MAX(0,y), im.h -1);
+
+    return im.data[get_index(im, x, y, c)];
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+    // Check bounds
+    if (x < 0 || x >= im.w || y < 0 || y >= im.h || c < 0 || c >= im.c) return;
+
+    // modify value
+    im.data[get_index(im, x, y, c)] = v;
 }
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
-    // TODO Fill this in
+    // use memcpy to copy data array
+    memcpy(copy.data, im.data, im.w * im.h * im.c * sizeof(float));
+
     return copy;
 }
 
@@ -60,4 +75,11 @@ void rgb_to_hsv(image im)
 void hsv_to_rgb(image im)
 {
     // TODO Fill this in
+}
+
+// Index Helper function, 
+// finds the index given an image, x and y
+int get_index(image im, int x, int y, int c)
+{
+    return x + im.w * y + im.w * im.h * c;
 }
--- tryhw0.py
+++ tryhw0.py
@@ -7,6 +7,11 @@ for row in range(im.h):
         set_pixel(im, col, row, 0, 0)
 save_image(im, "dog_no_red")
 
+#2 Copying image
+im = load_image("data/dog.jpg")
+cpy = copy_image(im)
+save_image(cpy, "dog_cpy")
+
 # 3. Grayscale image
 im = load_image("data/colorbar.png")
 graybar = rgb_to_grayscale(im)
